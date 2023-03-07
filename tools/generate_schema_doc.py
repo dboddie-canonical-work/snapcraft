76bf3045df38b6a80ae1f5c32d9a8d6a138318be
David Boddie <david.boddie@canonical.com>
2023-03-07 19:13:18 +0100

    Wrangled more output out of the schema.

--- tools/generate_schema_doc.py
+++ tools/generate_schema_doc.py
@@ -6,21 +6,22 @@ import sys
 def indent(level):
     print('  ' * level, end='')
 
-def print_type(types, level):
+def typestring(types):
     l = [typenames[t] for t in types]
-    if l:
-        print(' *or* '.join(l), end='')
+    return ' *or* '.join(l)
 
 def process_array(name, d, level):
     min_items = d.get('minItems', 1)
     unique = {False: ' ', True: 'unique '}[d.get('uniqueItems', True)]
+    indent(level)
+    print('   *with at least %i %sitem(s) of*' % (min_items, unique))
     if 'items' in d:
         items = d.get('items')
         if type(items) == dict:
-            process_schema(name, items, level)
+            process_schema(None, items, level)
+        elif len(items) == 1:
+            process_schema(None, items[0], level)
         else:
-            indent(level)
-            print('   *with at least %i %sitem(s) of*' % (min_items, unique))
             process_sequence(items, level)
 
 def process_boolean(name, d, level):
@@ -45,7 +46,7 @@ def process_object(name, d, level):
         print()
 
 def process_ref(name, d, level):
-    return
+    print('ref: ``%s``' % d['$ref'])
 
 def process_sequence(items, level):
     #indent(level)
@@ -82,9 +83,13 @@ typenames = {
 def process_schema(name, d, level):
     if level >= 0:
         indent(level)
-        print(' * ', end='')
         if name:
+            print(' * ', end='')
             print('``%s`` ' % name, end='')
+        elif name == '':
+            print(' * ', end='')
+        else:
+            print('   ', end='')
 
     if 'type' in d:
         if type(d['type']) == list:
@@ -92,13 +97,16 @@ def process_schema(name, d, level):
         else:
             types = [d['type']]
 
-        print_type(types, level + 1)
+        s = typestring(types)
+        if s:
+            print(s, end='')
+        elif not name:
+            print('*object with properties*')
         print()
 
         for t in types:
             handlers[t](name, d, level)
     elif '$ref' in d:
-        print('(ref)')
         process_ref(name, d, level)
     elif 'allOf' in d:
         print('*type must match all of these*')
